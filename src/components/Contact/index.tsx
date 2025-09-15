9f2838e8bdbbad580ff642d21b32b494562c0aa1
Lucas Lourenço <lucas.dev.profissional2001@gmail.com>
2025-09-15 11:46:44 -0300

    blank-gmail

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -6,27 +6,33 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Contact() {
   return (
-    
-      <div className=" flex gap-3 rounded-lg mt-3 lg:text-3xl md:text-2xl pb-2 mb-2">
-        <a href="https://github.com/Lucas-Lourenco " target="_blank" rel="noopener noreferrer" className="icons">
+
+    <div className=" flex gap-3 rounded-lg mt-3 lg:text-3xl md:text-2xl pb-2 mb-2">
+      <a href="https://github.com/Lucas-Lourenco " target="_blank" rel="noopener noreferrer" className="icons">
         <FaGithub />
       </a>
 
-     
+
       <a href="https://wa.me/5531995889036" target="_blank" rel="noopener noreferrer" className="icons">
         <FaWhatsapp />
       </a>
 
-      
+
       <a href="https://linkedin.com/in/lucas-lourenço-a88254302" target="_blank" rel="noopener noreferrer" className="icons">
         <BsLinkedin />
       </a>
 
-      
-      <a href="mailto:lucas.dev.profissional2001@gmail.com"
-      target="_blank" className="icons">
+
+      <a
+        href="mailto:lucas.dev.profissional2001@gmail.com"
+        className="icons"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <SiGmail />
       </a>
-            
-        </div>
-    );}
+
+
+    </div>
+  );
+}
