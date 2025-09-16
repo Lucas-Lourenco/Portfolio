2bc627308a226e45eacd3a2a6cec78d3d81def72
Lucas Lourenço <lucas.dev.profissional2001@gmail.com>
2025-09-16 08:24:50 -0300

    gmail-web

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,17 +1,28 @@
-
 import { FaWhatsapp } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 
 export default function Contact() {
-  return (
+  
+  const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(
+    navigator.userAgent
+  );
 
-    <div className=" flex gap-3 rounded-lg mt-3 lg:text-3xl md:text-2xl pb-2 mb-2">
-      <a href="https://github.com/Lucas-Lourenco " target="_blank" rel="noopener noreferrer" className="icons">
+  const mailLink = isMobile
+    ? "mailto:lucas.dev.profissional2001@gmail.com?subject=Olá%20Lucas&body=Vim%20pelo%20seu%20portfólio"
+    : "https://mail.google.com/mail/?view=cm&fs=1&to=lucas.dev.profissional2001@gmail.com&su=Olá%20Lucas&body=Vim%20pelo%20seu%20portfólio";
+
+  return (
+    <div className="flex gap-3 rounded-lg mt-3 lg:text-3xl md:text-2xl pb-2 mb-2">
+      <a
+        href="https://github.com/Lucas-Lourenco"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icons"
+      >
         <FaGithub />
       </a>
-
 
       <a
         href="https://wa.me/5531995889036?text=Ol%C3%A1%2C%20vim%20pelo%20seu%20portf%C3%B3lio"
@@ -22,23 +33,23 @@ export default function Contact() {
         <FaWhatsapp />
       </a>
 
-
-
-      <a href="https://linkedin.com/in/lucas-lourenço-a88254302" target="_blank" rel="noopener noreferrer" className="icons">
+      <a
+        href="https://linkedin.com/in/lucas-lourenço-a88254302"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icons"
+      >
         <BsLinkedin />
       </a>
 
-
       <a
-        href="mailto:lucas.dev.profissional2001@gmail.com"
-        className="icons"
-        target="_blank"
+        href={mailLink}
+        target={isMobile ? "_self" : "_blank"}
         rel="noopener noreferrer"
+        className="icons"
       >
         <SiGmail />
       </a>
-
-
     </div>
   );
 }
