60dd989613952191fcf0276aedf7c7e5038af22c
Lucas Lourenço <lucas.dev.profissional2001@gmail.com>
2025-11-26 13:19:21 -0300

    função

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -13,7 +13,7 @@ export default function About() {
                 <div className='flex flex-col justify-center items-center lg:items-start '>
                    
                     <h2 className="text-4xl sm:text-5xl lg:text-[4.25rem] font-bold">Lucas Lourenço</h2>
-                    <p className='text-2xl sm:text-3xl lg:text-4xl mt-2'>Desenvolvedor Frontend</p>
+                    <p className='text-2xl sm:text-3xl lg:text-4xl mt-2'>Desenvolvedor Web FullStack</p>
                     <Contact/>
                 </div>
                 <div>
@@ -37,7 +37,7 @@ export default function About() {
                 <div className='w-full lg:w-1/2 flex flex-col'>
                    
                     <p className='text-lg lg:text-xl text-justify mb-6'>
-                        Olá! Meu nome é Lucas Lourenço e sou um desenvolvedor frontend e engenheiro de software em formação. Tenho inglês avançado e uma paixão imensa por tecnologia, e tenho certeza que posso agregar valor a qualquer equipe com minhas habilidades.
+                        Olá! Meu nome é Lucas Lourenço e sou um desenvolvedor FullStack e engenheiro de software em formação. Tenho inglês avançado e uma paixão imensa por tecnologia, e tenho certeza que posso agregar valor a qualquer equipe com minhas habilidades.
                         Estou sempre me desafiando com novos projetos e participando de comunidades de programação para buscar feedback de outros desenvolvedores. Além de ajudar outras pessoas com o que já aprendi, gosto de usar o meu conhecimento para encontrar soluções criativas e eficientes para problemas.
                         Nas horas vagas, sou um grande entusiasta de filmes, séries, animes, jogos e pela cultura geek em geral.
                     </p>
